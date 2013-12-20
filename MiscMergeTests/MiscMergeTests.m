8c53f64f92a676cdeb9aafd3461318192ab5ded4
David Aspinall <daspinall@apple.com>
2013-12-20 08:08:59 -0700

    Preparing to upgrade to modern ObjC, especially ARC - Rearrange directory structure and moved files - create test directory

---
+++ MiscMergeTests/MiscMergeTests.m
@@ -0,0 +1,34 @@
+//
+//  MiscMergeTests.m
+//  MiscMergeTests
+//
+//  Created by David Aspinall on 12/20/2013.
+//  Copyright (c) 2013 Global Village Consulting. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MiscMergeTests : XCTestCase
+
+@end
+
+@implementation MiscMergeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
